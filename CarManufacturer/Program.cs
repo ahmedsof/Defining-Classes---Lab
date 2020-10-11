7f987f8ceb70daf4efd53a9c15f777942fb270dc
Ahmed Softev <ahmed_v@abv.bg>
2020-10-11 20:31:38 +0300

    C#

---
+++ CarManufacturer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "Mitsubishi";
+            car.Year = 19;
+            car.Model = "Space Star";
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
